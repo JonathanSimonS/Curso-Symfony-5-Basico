6224d1afaab837298d5d2de99a31d6f3e194fec7
Jonathan Simon S <yonidefines@hotmail.com>
2022-06-04 00:52:26 +0200

    Eliminar tarea añadido

--- src/Controller/TareaController.php
+++ src/Controller/TareaController.php
@@ -147,11 +147,83 @@ class TareaController extends AbstractController
     }
 
     // CREO LA FUNCION ELIMINAR
-    #[Route('/tarea/eliminar/{id}', name: 'app_eliminar_tarea')]
-    public function eliminar(int $id): Response
+    /**
+     * @Route(
+     * "/tarea/eliminar/{id}", 
+     * name="app_eliminar_tarea", 
+     * requirements={"id"="\d+"}
+     * )
+     */
+    public function eliminar(Tarea $tarea,ManagerRegistry $doctrine): Response
     {
-        return $this->render('tarea/eliminar.html.twig', [
-            'controller_name' => 'TareaController',
+        $em = $doctrine->getManager(); //entityManager
+        $em -> remove($tarea);
+        $em ->flush(); // para que se ejcute en bbdd
+        $this->addFlash(
+            'success',
+            '¡Tarea eliminada correctamente!'
+        );
+        // solo redirigimos
+        return $this->redirectToRoute('app_listado_tarea');
+    }
+    /**
+     * @Route(
+     * "/tarea/editar-params/{id}", 
+     * name="app_editar_tarea_con_params_convert", 
+     * requirements={"id"="\d+"}
+     * )
+     */
+    
+    // en los parámetros indicar primero los nuestros (apreciación)
+    // editando una entidad, por lo que la debemos recoger con TareaRepository
+
+    // FORMA RECOMENDADA
+    // automáticamente nos busca la tarea con id idéntica, sin pasarselo por parámetro
+    public function editarConParamsConvert(Tarea $tarea, TareaRepository $tareaRepository, Request $request, ManagerRegistry $doctrine, ValidatorInterface $validator): Response
+    {
+
+        // si no se encuentra, automaticamente devolverá la excepción, no hace falta
+        // if (null === $tarea) {
+        //     throw $this->createNotFoundException();
+        // }
+
+        // obtenemos la descripcion mediante request | query si fuese GET ($request->query->get('descripcion');)
+        $descripcion = $request->request->get('descripcion', null); // si no existe devuelve null        
+
+        if (null !== $descripcion) {
+            if (!empty($descripcion)) {
+
+                //obtiene el objeto administrador de entidades de Doctrine, que es el objeto más importante de Doctrine.
+                //responsable de guardar y recuperar objetos de la base de datos.
+                $em = $doctrine->getManager(); //entityManager
+
+                $tarea->setDescripcion($descripcion);
+
+                // ejecuta un INSERT
+                $em->flush();
+
+                // mensaje flash
+                $this->addFlash('success','¡Tarea editada correctamente!' );
+
+                // finamente la redirijo al listado
+                return $this->redirectToRoute('app_listado_tarea');
+
+            }else {
+                // mensaje flash
+                $this->addFlash(
+                    'warning',
+                    'El campo "Descripción" es obligatorio'
+                );
+            }
+        }
+
+        $errors = $validator->validate($tarea);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
+
+        return $this->render('tarea/editar.html.twig', [
+            "tarea" => $tarea,
         ]);
     }
 }
